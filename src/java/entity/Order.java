10ec3d40aed7a28ba7987c6ce230d23aed6c64c9
HieuNonTay <hieubuivan646@gmail.com>
2024-06-06 13:53:12 +0700

    order

--- src/java/entity/Order.java
+++ src/java/entity/Order.java
@@ -4,71 +4,23 @@
  */
 package entity;
 
+import java.sql.Timestamp;
+
 /**
  *
  * @author DELL
  */
 public class Order {
-    private String OrderID, AccountID, OrderDate, Total, Address, Status;
+
+    private int orderId, accountId;
+    private String firstName, lastName;
+    private Timestamp Orderdate;
+    private double total;
+    private String line1, line2, city, province, countryId;
+    private Timestamp createdAt, updateAt;
+    private String payment, status;
 
     public Order() {
     }
 
-    public Order(String OrderID, String AccountID, String OrderDate, String Total, String Address, String Status) {
-        this.OrderID = OrderID;
-        this.AccountID = AccountID;
-        this.OrderDate = OrderDate;
-        this.Total = Total;
-        this.Address = Address;
-        this.Status = Status;
-    }
-
-    public String getOrderID() {
-        return OrderID;
-    }
-
-    public void setOrderID(String OrderID) {
-        this.OrderID = OrderID;
-    }
-
-    public String getAccountID() {
-        return AccountID;
-    }
-
-    public void setAccountID(String AccountID) {
-        this.AccountID = AccountID;
-    }
-
-    public String getOrderDate() {
-        return OrderDate;
-    }
-
-    public void setOrderDate(String OrderDate) {
-        this.OrderDate = OrderDate;
-    }
-
-    public String getTotal() {
-        return Total;
-    }
-
-    public void setTotal(String Total) {
-        this.Total = Total;
-    }
-
-    public String getAddress() {
-        return Address;
-    }
-
-    public void setAddress(String Address) {
-        this.Address = Address;
-    }
-
-    public String getStatus() {
-        return Status;
-    }
-
-    public void setStatus(String Status) {
-        this.Status = Status;
-    }
-    
 }
